2f88927e4752575c18e47dd10244a4bb7dd49b21
mohd saqib <mohd.saqib@squareboat.com>
2023-08-30 18:54:38 +0530

    deployed on firebase

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import {FastifyAdapter, NestFastifyApplication} from '@nestjs/platform-fastify'
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import * as functions from 'firebase-functions';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
@@ -13,15 +13,8 @@ async function bootstrap() {
       transform: true,
     }),
   );
-  const config = new DocumentBuilder()
-  .setTitle('Tasks REST API')
-  .setDescription('The fuelbuddy task API assignment')
-  .setVersion('1.0')
-  .addTag('task')
-  .build();
-const document = SwaggerModule.createDocument(app, config);
 
-SwaggerModule.setup('api', app, document);
   await app.listen(3000, '0.0.0.0');
 }
 bootstrap();
+export const api = functions.https.onRequest(bootstrap);
