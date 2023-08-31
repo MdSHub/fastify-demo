994cac7cbb96c510a28c311133e42c4e6556a8d6
mohd saqib <mohd.saqib@squareboat.com>
2023-08-31 11:50:55 +0530

    mmodified main.ts for firebase deployment

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,48 @@
+// import { NestFactory } from '@nestjs/core';
+// import {FastifyAdapter, NestFastifyApplication} from '@nestjs/platform-fastify'
+// import { AppModule } from './app.module';
+// import { ValidationPipe } from '@nestjs/common';
+// import * as functions from 'firebase-functions';
+
+// async function bootstrap() {
+//   const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
+//   app.useGlobalPipes(
+//     new ValidationPipe({
+//       whitelist: true,
+//       forbidNonWhitelisted: true,
+//       transform: true,
+//     }),
+//   );
+
+//   await app.listen(process.env.PORT || 3000, '0.0.0.0');
+// }
+// bootstrap();
+// export const api = functions.https.onRequest(bootstrap);
+
 import { NestFactory } from '@nestjs/core';
-import {FastifyAdapter, NestFastifyApplication} from '@nestjs/platform-fastify'
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
+import * as functions from 'firebase-functions';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import * as functions from 'firebase-functions';
 
-async function bootstrap() {
-  const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      forbidNonWhitelisted: true,
-      transform: true,
-    }),
+
+const expressServer = express();
+const createFunction = async (expressInstance): Promise<void> => {
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(expressInstance),
   );
-
-  await app.listen(3000, '0.0.0.0');
-}
-bootstrap();
-export const api = functions.https.onRequest(bootstrap);
+  app.useGlobalPipes(
+        new ValidationPipe({
+          whitelist: true,
+          forbidNonWhitelisted: true,
+          transform: true,
+        }),
+      );
+await app.init();
+};
+export const api = functions.https.onRequest(async (request, response) => {
+  await createFunction(expressServer);
+  expressServer(request, response);
+});
